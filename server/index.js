5cb7681b793f92c7f408d9a3a4eddb4e8e2844b8
SIDIKI F <39869497+sidikif@users.noreply.github.com>
2020-01-17 00:56:40 +0000

    Update Port env

--- server/index.js
+++ server/index.js
@@ -22,6 +22,6 @@ if (process.env.NODE_ENV === 'production') {
     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
